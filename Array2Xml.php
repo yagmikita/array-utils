22d5b80c33e78712d40ac9fffbafecb26b66106b
Nikita Gopkalo <yagmikita@gmail.com>
2013-08-29 09:58:37 +0300

    Fix [idents]: resaved in IDE for proper view

--- Array2Xml.php
+++ Array2Xml.php
@@ -2,54 +2,55 @@
 
 class Array2Xml
 {
-	protected $res;
-	protected $context;
+    protected $res;
+    protected $context;
     protected $charset;
 
     public function __construct(array $context, $charset = 'UTF-8')
     {
         if (count($context) != 1) {
-			throw new Exception('There must be only one root element', 500);
+            throw new Exception('There must be only one root element', 500);
         }
-		$this->context = $context;
+        $this->context = $context;
         $this->charset = $charset;
         $this->init(key($this->context));
     }
 
-	public function conv() {
-		return $this->formatXml($this->a2x(current($this->context), $this->res));
-	}
+    public function conv()
+    {
+        return $this->formatXml($this->a2x(current($this->context), $this->res));
+    }
 
-	protected function init($root)
+    protected function init($root)
     {
         $header = "<?xml version=\"1.0\" encoding=\"{$this->charset}\"?><{$root}/>";
-		$this->res = new SimpleXMLElement($header);
-	}
+        $this->res = new SimpleXMLElement($header);
+    }
 
-	protected function addAttribs(array $attribs, SimpleXMLElement $node)
+    protected function addAttribs(array $attribs, SimpleXMLElement $node)
     {
         if (count($attribs)) {
-        	foreach ($attribs as $attrKey => $attrVal) {
-				$node->addAttribute($attrKey, $attrVal);
-        	}
+            foreach ($attribs as $attrKey => $attrVal) {
+                $node->addAttribute($attrKey, $attrVal);
+            }
         }
         return $node;
     }
 
-	protected function tryToAddAttribs(array $inner, SimpleXMLElement $node)
+    protected function tryToAddAttribs(array $inner, SimpleXMLElement $node)
     {
-		if (isset($inner['@attributes'])) {
+        if (isset($inner['@attributes'])) {
             $this->addAttribs($inner['@attributes'], $node);
             unset($inner['@attributes']);
         }
-		return $node;
+        return $node;
     }
 
-	protected function addEnum(array $items, SimpleXMLElement $node, $caption)
+    protected function addEnum(array $items, SimpleXMLElement $node, $caption)
     {
         if (count($items)) {
             foreach ($items as $item) {
-				$subNode = $node->addChild($caption);
+                $subNode = $node->addChild($caption);
                 $this->tryToAddAttribs($item, $subNode);
                 unset($item['@attributes']);
             }
@@ -58,13 +59,13 @@ class Array2Xml
 
     protected function a2x(array $inner, SimpleXMLElement $node)
     {
-		$this->tryToAddAttribs($inner, $node);
+        $this->tryToAddAttribs($inner, $node);
         unset($inner['@attributes']);       
         if (count($inner)) {
-			foreach ($inner as $key => $value) {
+            foreach ($inner as $key => $value) {
                 if ($this->isEnumeration($value)) {
                     $this->addEnum($value, $node, $key);
-    			} else {
+                } else {
                     if (isset($value['@content'])) {
                         $subNode = $node->addChild($key, $value['@content']);
                     } else {
@@ -72,20 +73,20 @@ class Array2Xml
                     }
                     unset($value['@content']);
                     if (count($value)) {
-                	   $this->a2x($value, $subNode);
+                       $this->a2x($value, $subNode);
                     }
-        		}
+                }
             }
         }
         return $this->res;
     }
 
-	protected function isEnumeration(array $items)
+    protected function isEnumeration(array $items)
     {
-		$keys = array_keys($items);
+        $keys = array_keys($items);
         foreach ($keys as $key) {
-			if (!is_numeric($key)) {
-				return false;
+            if (!is_numeric($key)) {
+                return false;
             }
         }
         return true;
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 array2xml
 =========
+
+Array2Xml class uses SimpleXMLElement class to recursivly convert php associative array into xml-formatted string.
+
+ * in order to to add attributes - add key '@attributes', ex.:
+ 	<?php
+ 		$a = [
+ 			'root' => [
+ 				'@attributes' => [
+ 					'attr1' => '1',
+ 				],
+ 			],
+ 		];
+
+ * in order to add text content to the node - add key '@content', ex.:
+ 	<?php
+ 		$a = [
+ 			'root' => [
+ 				'@content' => 'This is text content',
+ 			],
+ 		];
+
+ * in order to add tags with the same name add an array of the enumerated elements with the key of desired keys names, ex.:
+ 	<?php
+ 		$a = [
+ 			'root' => [
+ 				'people' => [
+ 					'person' => [
+ 						[
+ 							'@content' => 'Alex'
+ 						],
+ 						[
+ 							'@content' => 'Max'
+ 						],
+ 						[
+ 							'@content' => 'Eugen'
+ 						],
+ 					],
+ 				],
+ 			],
+ 		];
